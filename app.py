9bc66895af24fe479b537610445a2ba8d21a6e69
Godswill omuka <omukagodswil@gmail.com>
2025-03-18 16:03:16 +0300

    prepare for render deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS  # Import CORS to handle cross-origin requests
@@ -6,7 +7,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Configure PostgreSQL connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dripkidzilla@localhost/task_manager'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
